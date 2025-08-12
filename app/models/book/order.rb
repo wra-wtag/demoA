d3fd22b60cbae37fae69f1d5e86b9faedebe1b02
Wasi Ur Rahman <wasiurrahman@Wasis-Mac-mini.local>
2025-08-12 13:49:53 +0600

    Added validations to the records

--- app/models/book/order.rb
+++ app/models/book/order.rb
@@ -1,2 +1,4 @@
 class Book::Order < ApplicationRecord
+    validates :quantity, presence: true
+    validates :quantity, numericality: { only_integer: true, greater_than: 0 }
 end
--- app/models/book/review.rb
+++ app/models/book/review.rb
@@ -1,2 +1,5 @@
 class Book::Review < ApplicationRecord
+    validates :title, presence: true
+    validates :content, presence: true
+    validates :rating, presence: true, inclusion: { in: 1..5 }
 end
