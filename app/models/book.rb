cf58471aafe71c637252a42236c98e1e43dad21d
Wasi Ur Rahman <wasiurrahman@Wasis-Mac-mini.local>
2025-08-12 14:17:19 +0600

    Added callback upon book creation

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,9 @@ class Book < ApplicationRecord
     has_many :reviews, class_name: "Book::Review", dependent: :destroy
     validates :title, presence: true
     validates :author, presence: true
+    after_create :log_new_book
+
+    def log_new_book
+        puts "📚 Book '#{self.title}' has been created."
+    end
 end
