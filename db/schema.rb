a858f3528b2529617e478b3fbcf2ac81b08aaf00
Wasi Ur Rahman <wasiurrahman@Wasis-Mac-mini.local>
2025-08-13 14:08:00 +0600

    Added a user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_13_080629) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_13_080734) do
   create_table "book_orders", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -43,6 +43,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_13_080629) do
     t.integer "stock_quantity"
     t.decimal "price"
     t.index ["price"], name: "index_products_on_price"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "book_orders", "books"
