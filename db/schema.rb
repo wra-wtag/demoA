03a75f5f1007f5f5797ff6f4149035c80c217897
Wasi Ur Rahman <wasiurrahman@Wasis-Mac-mini.local>
2025-08-13 14:06:56 +0600

    removed an existing column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_13_080506) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_13_080629) do
   create_table "book_orders", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -38,7 +38,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_13_080506) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.string "part_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "stock_quantity"
